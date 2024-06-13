3a8a56844cb117cfe34c90460112ce2856a3f89b
free-man11 <pfreeman@st.ug.edu.gh>
2024-06-13 22:36:42 +0000

    Add Inheritance and Method Overriding demo

--- AbstractClass/obj/Debug/net8.0/AbstractClass.AssemblyInfo.cs
+++ AbstractClass/obj/Debug/net8.0/AbstractClass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbstractClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b196d7dadc86c47b66232b056f1788e49569d8f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b2569f643aa1efb2e6e80885138812ebec099b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbstractClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbstractClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
