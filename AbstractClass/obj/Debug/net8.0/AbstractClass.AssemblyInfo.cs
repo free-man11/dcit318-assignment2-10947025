9f7a7b69a8b4b785d9be9dc8c32c5ea6b59f4188
free-man11 <pfreeman@st.ug.edu.gh>
2024-06-13 23:20:32 +0000

    Add Interfaces

--- AbstractClass/obj/Debug/net8.0/AbstractClass.AssemblyInfo.cs
+++ AbstractClass/obj/Debug/net8.0/AbstractClass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbstractClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b2569f643aa1efb2e6e80885138812ebec099b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8a56844cb117cfe34c90460112ce2856a3f89b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbstractClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbstractClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
